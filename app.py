66ebdaee1ac7468936fed9dce486ae7335816cc2
李明叡 <leeray19901208@gmail.com>
2020-01-27 16:38:08 -0800

    Update

--- app.py
+++ app.py
@@ -43,6 +43,18 @@ def callback():
 # 依原樣 使用者傳hi, line_bot reply hi
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = 'Sorry, what do you mean?'
+
+    if msg in ['hi', 'Hi']:
+        r = 'Hi'
+    elif msg == 'Do you have meal?':
+        r = 'not yet'
+    elif msg == 'Who are you':
+        r = "I'm a robot"
+    elif 'reserve' in msg:
+        r = 'You want to reserve, right?'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
